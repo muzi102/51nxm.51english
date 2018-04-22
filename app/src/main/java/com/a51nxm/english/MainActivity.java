dcbb28ff7cd63fcdbb244a3a1c7b0da43f7b7c5d
panda102 <1565675791@qq.com>
2018-04-22 10:21:58 +0800

    frist commti

---
+++ app/src/main/java/com/a51nxm/english/MainActivity.java
@@ -0,0 +1,14 @@
+package com.a51nxm.english;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //我只是改动了一下下
+    }
+}
